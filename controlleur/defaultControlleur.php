58240fec811bca0c7a57086744f9c3f71a379e0e
Mr-Personne <h.sacha07@gmail.com>
2020-01-28 11:45:35 +0000

    changement systeme  avec routeur vers pages test

--- controlleur/defaultControlleur.php
+++ controlleur/defaultControlleur.php
@@ -6,14 +6,17 @@ print_r($_POST);
 
 function defaultAction()
 {
+    $action = 'default';
+    
     if(isset($_POST['identifiant'])){
         $user = getUser($_POST['identifiant']);
         print_r($user);
-        checkUser($user);
+        $action = checkUser($user);
     }
     
     
-    require_once 'views/login-test.html.twig';
+    // require_once 'views/login-test.html.twig';
+    return $action;
     
 }
 
@@ -22,17 +25,26 @@ function checkUser($user){
     //si getUser() ne trouve aucun utilisateur avec cette identifiant :
     if($user == ""){
         echo 'aucun utilisateur trouvé';
+        return "default";
     }
-    else if($user['mdp'] == $_POST['mdp']){
-        echo 'Bon mot de passe!';
+    else if($user['mdp'] == $_POST['mdp'] && $user['admin'] == "true"){
+        echo 'Bon mot de passe admin!';
+        return "admin";
+    }
+    else if($user['mdp'] == $_POST['mdp'] && $user['admin'] == "false"){
+        echo 'Bon mot de passe employe!';
+        return "employe";
     }
     else{
         echo 'mauvais mdp';
+        return "default";
     }
 }
 
 
-$action = 'default';
+
+$action = defaultAction();
+echo $action;
 
 if (strpos($uri, '/', 1) !== false) {
     $action = (strpos($uri, '/', strlen($controlleur) + 1)  === false) ? substr($uri, strpos($uri, '/', strlen($controlleur))) : substr($uri,  strlen($controlleur) + 1, (strpos($uri, '/', strlen($controlleur) + 1) - 1) - (strlen($controlleur) - 1) - 1);
@@ -41,10 +53,16 @@ if (strpos($uri, '/', 1) !== false) {
 switch ($action) {
     case 'default':
     case "";
-        defaultAction();
+        // defaultAction();
+        require_once 'views/login-test.html.twig';
         break;
-    case 'detail';
-        // detailAction();
+    case 'admin';
+        // defaultAction();
+        require_once 'views/admin-test.html.twig';
+        break;
+    case 'employe';
+        // defaultAction();
+        require_once 'views/utilisateur.html.twig';
         break;
     default:
     require_once 'views/404.html.php' ;
