29624a49c841fe9e145775adb57dabfa26a40ed4
Mr-Personne <h.sacha07@gmail.com>
2020-01-29 21:48:54 +0000

    test fonction humeurAnneeTotal(2020) et tests echo

--- controlleur/adminControlleur.php
+++ controlleur/adminControlleur.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'vendor/autoload.php';
-// require_once 'core/db.php';
-// require_once 'model/getUser.php';
+require_once 'core/db.php';
+require_once 'model/getHumeurTotal.php';
 echo "ENTER ADMIN";
 print_r($_POST);
 // print_r($_SESSION);
@@ -10,9 +10,14 @@ print_r($_POST);
 // $twig = new \Twig\Environment($loader);
 // // $template = $twig->load('admin-test.html.twig');
 // echo $twig->render('admin-test.html.twig', ['var1' => 'variables', 'var2' => 'here']);
+$allyeardata = humeurAnneeTotal(2020);
 $action = "default";
 switch ($action) {
     case 'default':
+        // print_r($allyeardata);
+        // echo $allyeardata[0]['vote_total'];
+        // echo $allyeardata[1]['vote_total'];
+        // echo $allyeardata[2]['vote_total'];
         require_once 'views/admin.html';
         break;
 
