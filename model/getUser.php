2fc5bb79c1418646f8a0d9e9126f6a18f3c946e6
Mr-Personne <h.sacha07@gmail.com>
2020-01-29 19:47:32 +0000

    modif sql var (identifiant + mdp)

--- model/getUser.php
+++ model/getUser.php
@@ -3,20 +3,21 @@
 
 // function getAllUser(){
 //     global $pdo;
-    // $sql = "INSERT INTO Humeur (nom_humeur, date) VALUES ($clickedHumeur, $getDate)";
+// $sql = "INSERT INTO Humeur (nom_humeur, date) VALUES ($clickedHumeur, $getDate)";
 //     $sth = $pdo->prepare($sql);
 //     $sth->execute();
 //     return $sth->fetchAll(pdo::FETCH_ASSOC);
 // }
-function getUser($id, $mdp){
+function getUser($id, $mdp)
+{
     global $pdo;
     // $sql = "SELECT * FROM Employe WHERE utilisateur =:id";
     $sql = "SELECT * FROM `Employe` WHERE utilisateur = :id AND mdp = PASSWORD( :mdp )";
     // SELECT * FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur
+    // SELECT *, count(nom_humeur) AS vote_total FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur GROUP BY nom_humeur
     $sth = $pdo->prepare($sql);
-    $sth->bindParam(':id',$id, PDO::PARAM_STR);
-    $sth->bindParam(':mdp',$mdp, PDO::PARAM_STR);
+    $sth->bindParam(':id', $id, PDO::PARAM_STR);
+    $sth->bindParam(':mdp', $mdp, PDO::PARAM_STR);
     $sth->execute();
     return $sth->fetch(pdo::FETCH_ASSOC);
 }
-
