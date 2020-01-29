62e21961456d2af1bc89b9b4682e155bd23dfbfe
Mr-Personne <h.sacha07@gmail.com>
2020-01-29 19:44:26 +0000

    modif sql var (identifiant + mdp)

--- model/getUser.php
+++ model/getUser.php
@@ -8,13 +8,14 @@
 //     $sth->execute();
 //     return $sth->fetchAll(pdo::FETCH_ASSOC);
 // }
-function getUser($id){
+function getUser($id, $mdp){
     global $pdo;
     // $sql = "SELECT * FROM Employe WHERE utilisateur =:id";
-    $sql = "SELECT * FROM Employe WHERE utilisateur = :id";
+    $sql = "SELECT * FROM `Employe` WHERE utilisateur = :id AND mdp = PASSWORD( :mdp )";
     // SELECT * FROM `Employe` WHERE utilisateur = :id AND mdp = PASSWORD( :mdp )
     $sth = $pdo->prepare($sql);
     $sth->bindParam(':id',$id, PDO::PARAM_STR);
+    $sth->bindParam(':mdp',$mdp, PDO::PARAM_STR);
     $sth->execute();
     return $sth->fetch(pdo::FETCH_ASSOC);
 }
