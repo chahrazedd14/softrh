49b501fa996eedf081c28f7df5623e9c1e8f6d36
Mr-Personne <h.sacha07@gmail.com>
2020-01-30 14:00:13 +0000

    tests fonctions humeurXTotal() par secteur par jour

--- controlleur/adminControlleur.php
+++ controlleur/adminControlleur.php
@@ -2,8 +2,8 @@
 require_once 'vendor/autoload.php';
 require_once 'core/db.php';
 require_once 'model/getHumeurTotal.php';
-echo "ENTER ADMIN";
-print_r($_POST);
+// echo "ENTER ADMIN";
+// print_r($_POST);
 // print_r($_SESSION);
 
 // $loader = new \Twig\Loader\FilesystemLoader('views');
@@ -11,33 +11,57 @@ print_r($_POST);
 // // $template = $twig->load('admin-test.html.twig');
 // echo $twig->render('admin-test.html.twig', ['var1' => 'variables', 'var2' => 'here']);
 $currDate = explode("-", date("Y-m-d"));
-print_r($currDate);
+// print_r($currDate);
 
 $allyeardata = humeurAnneeTotal("2020");
-$allMonthData = humeurMoisTotal("1","2020");
-$allJourData = humeurJourTotal($currDate[2],$currDate[1],$currDate[0]);
-$janvComptaTotalHumeur = humeurMoisTotalService("1","2020", "comptabilite");
-$janvsecretariatTotalHumeur = humeurMoisTotalService("1","2020", "secretariat");
-$janvlogistiqueTotalHumeur = humeurMoisTotalService("1","2020", "logistique");
-$janvjuridiqueTotalHumeur = humeurMoisTotalService("1","2020", "juridique");
- 
+$allMonthData = humeurMoisTotal("1", "2020");
+$allJourData = humeurJourTotal($currDate[2], $currDate[1], $currDate[0]);
+// $janvComptaTotalHumeur = humeurMoisTotalService("1","2020", "comptabilite");
+// $janvsecretariatTotalHumeur = humeurMoisTotalService("1","2020", "secretariat");
+// $janvlogistiqueTotalHumeur = humeurMoisTotalService("1","2020", "logistique");
+// $janvjuridiqueTotalHumeur = humeurMoisTotalService("1","2020", "juridique");
+$allMonthDataParService = humeurMoisTotalParService();
+$humeurMoisParJourTotal = humeurMoisParJourTotal();
+
 $action = "default";
+
+if (!isset($_POST['selectedService'])) {
+    $action = "default";
+} else {
+    $action = "service" . $humeurMoisParJourTotal;
+}
+
+
 switch ($action) {
     case 'default':
         // print_r($allyeardata);
-        print_r($allMonthData);
-        print_r($allJourData);
-        echo "compta";
-        print_r($janvComptaTotalHumeur);
-        echo "secre";
-        print_r($janvsecretariatTotalHumeur);
-        echo "logis";
-        print_r($janvlogistiqueTotalHumeur);
-        echo "juri";
-        print_r($janvjuridiqueTotalHumeur);
-        // echo $allyeardata[0]['vote_total'];
-        // echo $allyeardata[1]['vote_total'];
-        // echo $allyeardata[2]['vote_total'];
+        // print_r($allMonthData);
+        // print_r($allJourData);
+        // echo "compta";
+        // print_r($janvComptaTotalHumeur);
+        // echo "secre";
+        // print_r($janvsecretariatTotalHumeur);
+        // echo "logis";
+        // print_r($janvlogistiqueTotalHumeur);
+        // echo "juri";
+        // print_r($janvjuridiqueTotalHumeur);
+        // echo "allMonthDataParService";
+        // print_r($allMonthDataParService);
+
+        // echo "humeurMoisParJourTotal";
+        // print_r($humeurMoisParJourTotal);
+
+        echo json_encode($humeurMoisParJourTotal);
+        require_once 'views/admin.html';
+        break;
+
+
+    case 'service1':
+        $humeurMoisParJourTotal = humeurMoisParJourTotal();
+        echo "humeurMoisParJourTotal";
+        print_r($humeurMoisParJourTotal);
+
+        echo json_encode($humeurMoisParJourTotal);
         require_once 'views/admin.html';
         break;
 
