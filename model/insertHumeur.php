7747ba1d58692f62facd65d7926a6ad201ff83ee
momo1080 <karim-faysel@outlook.fr>
2020-01-30 12:34:23 +0000

    moi

--- model/insertHumeur.php
+++ model/insertHumeur.php
@@ -1,30 +1,8 @@
 <!-- fonction par rapport requet  -->
 <?php
+INSERT INTO Vote (id_humeur, id_service, vote_date)
+VALUES (' ',' ',' ',' ',' ',' ');
 
-$serveur = "localhost";
-$dbname = "";
-$login = "root";
-$pass = "online@2017";
-
-try{
-    $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    //On prépare la requête et on l'exécute
-    $sth = $dbco->prepare("
-      UPDATE Users
-      SET =''
-      WHERE id=2
-    ");
-    $sth->execute();
-    
-    //On affiche le nombre d'entrées mise à jour
-    $count = $sth->rowCount();
-    print('Mise à jour de ' .$count. ' entrée(s)');
-}
-      
-catch(PDOException $e){
-    echo "Erreur : " . $e->getMessage();
-}
 ?>
 
 
