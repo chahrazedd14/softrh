6d697b8b1c056912d4050dc1f7959e7289353724
Mr-Personne <h.sacha07@gmail.com>
2020-02-05 12:16:37 +0000

    modif fonction humeurMoisTotalParService() pour que la date soit variable (bindParam)

--- model/getHumeurTotal.php
+++ model/getHumeurTotal.php
@@ -8,8 +8,8 @@ function humeurJourTotal($jour = "01", $mois = "1", $annee = "2020")
     // $sql = "SELECT * FROM Employe WHERE utilisateur =:id";
     $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur WHERE vote_date LIKE :date GROUP BY nom_humeur";
     // $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur WHERE vote_date LIKE '%-%1-%' GROUP BY nom_humeur";
-    
-    $selectedDate = "".$annee."-%".$mois."-".$jour."";
+
+    $selectedDate = "" . $annee . "-%" . $mois . "-" . $jour . "";
     $sth = $pdo->prepare($sql);
     $sth->bindParam(':date', $selectedDate, PDO::PARAM_STR);
     $sth->execute();
@@ -22,8 +22,8 @@ function humeurMoisParJourTotal($service = 1, $mois = "1", $annee = "2020")
     // $sql = "SELECT * FROM Employe WHERE utilisateur =:id";
     $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total, vote_date FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur INNER JOIN Service ON Vote.id_service = Service.id_service WHERE Service.id_service = :serviceNum AND vote_date LIKE :date GROUP BY nom_humeur, vote_date ORDER BY `Vote`.`vote_date` ASC";
     // $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur WHERE vote_date LIKE '%-%1-%' GROUP BY nom_humeur";
-    
-    $selectedDate = "".$annee."-%".$mois."-%";
+
+    $selectedDate = "" . $annee . "-%" . $mois . "-%";
     $sth = $pdo->prepare($sql);
     $sth->bindParam(':date', $selectedDate, PDO::PARAM_STR);
     $sth->bindParam(':serviceNum', $service, PDO::PARAM_INT);
@@ -38,8 +38,8 @@ function humeurMoisTotal($mois = "1", $annee = "2020")
     // $sql = "SELECT * FROM Employe WHERE utilisateur =:id";
     $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur WHERE vote_date LIKE :date GROUP BY nom_humeur";
     // $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur WHERE vote_date LIKE '%-%1-%' GROUP BY nom_humeur";
-    
-    $selectedDate = "".$annee."-%".$mois."-%";
+
+    $selectedDate = "" . $annee . "-%" . $mois . "-%";
     $sth = $pdo->prepare($sql);
     $sth->bindParam(':date', $selectedDate, PDO::PARAM_STR);
     $sth->execute();
@@ -54,8 +54,8 @@ function humeurMoisTotalService($mois = "1", $annee = "2020", $service = "compta
     // $sql = "SELECT * FROM Employe WHERE utilisateur =:id";
     $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total, nom_service FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur INNER JOIN Service ON Vote.id_service = Service.id_service WHERE vote_date LIKE :date AND nom_service = :service GROUP BY nom_humeur";
     // SELECT nom_humeur, count(nom_humeur) AS vote_total, nom_service FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur INNER JOIN Service ON Vote.id_service = Service.id_service WHERE vote_date LIKE '%-01-%' AND nom_service = 'comptabilite' GROUP BY nom_humeur
-    
-    $selectedDate = "".$annee."-%".$mois."-%";
+
+    $selectedDate = "" . $annee . "-%" . $mois . "-%";
     $sth = $pdo->prepare($sql);
     $sth->bindParam(':date', $selectedDate, PDO::PARAM_STR);
     $sth->bindParam(':service', $service, PDO::PARAM_STR);
@@ -64,17 +64,18 @@ function humeurMoisTotalService($mois = "1", $annee = "2020", $service = "compta
 }
 
 //retourne array avec total chaque humeur par services
-function humeurMoisTotalParService($mois = "1", $annee = "2020")
+function humeurMoisTotalParService($mois = "01", $annee = "2020")
 {
     global $pdo;
     // $sql = "SELECT * FROM Employe WHERE utilisateur =:id";
-    $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total, nom_service FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur INNER JOIN Service ON Vote.id_service = Service.id_service WHERE vote_date LIKE '%-01-%' GROUP BY nom_humeur, nom_service ORDER BY `Service`.`nom_service` ASC";
+    $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total, nom_service FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur INNER JOIN Service ON Vote.id_service = Service.id_service WHERE vote_date LIKE :date GROUP BY nom_humeur, nom_service ORDER BY `Humeur`.`nom_humeur` ASC, `Service`.`nom_service` ASC";
     // SELECT nom_humeur, count(nom_humeur) AS vote_total, nom_service FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur INNER JOIN Service ON Vote.id_service = Service.id_service WHERE vote_date LIKE '%-01-%' AND nom_service = 'comptabilite' GROUP BY nom_humeur
-    
-    $selectedDate = "".$annee."-%".$mois."-%";
+
+    $selectedDate = $annee . "-" . $mois . "-%";
+    // $selectedDate = "2020-01-%";
     $sth = $pdo->prepare($sql);
     $sth->bindParam(':date', $selectedDate, PDO::PARAM_STR);
-    $sth->bindParam(':service', $service, PDO::PARAM_STR);
+    // $sth->bindParam(':service', $service, PDO::PARAM_STR);
     $sth->execute();
     return $sth->fetchAll(pdo::FETCH_ASSOC);
 }
@@ -85,8 +86,8 @@ function humeurAnneeTotal($annee = "2020")
     global $pdo;
     $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur WHERE vote_date LIKE :date GROUP BY nom_humeur";
     // $sql = "SELECT nom_humeur, count(nom_humeur) AS vote_total FROM Vote INNER JOIN Humeur ON Vote.id_humeur = Humeur.id_humeur WHERE vote_date LIKE '%-%1-%' GROUP BY nom_humeur";
-    
-    $selectedYear = $annee.'-%-%';
+
+    $selectedYear = $annee . '-%-%';
     $sth = $pdo->prepare($sql);
     $sth->bindParam(':date', $selectedYear, PDO::PARAM_STR);
     $sth->execute();
