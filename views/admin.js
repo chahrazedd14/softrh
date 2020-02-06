5cca46a8f4f3f7850e6340793dfdce4e008bf67e
Mr-Personne <h.sacha07@gmail.com>
2020-02-06 12:22:42 +0000

    codes pour modif span à coté de 'Humeur du jour' (pour afficher nom service du graph en cours)

--- views/admin.js
+++ views/admin.js
@@ -47,106 +47,118 @@ function ajaxCallAsynch(service_id) {
     console.log("jourArray : ", jourArray);
 
 
-    //reset graphs
-    // addData(chart, jsonDataMois['data']['labels'], jsonDataMois['data']['datasets'][0]['data']);
-    // removeData(chart);
+    //change nom service affiché à coté de "Humeur du jour"
+    let nomsServiceArr = ["- Comptabilité", "- Juridique", "- Secrétariat", "- Logistique"];
+    let currentIdService = data['id_service'] - 1;
+    let serviceSpan = document.querySelector('.nom-service');
+    serviceSpan.innerText = nomsServiceArr[currentIdService];
 
     // startChart(jourArray, jsonDataJours);
     //chart ( mois )
-    var chart = document.getElementById('myChart');
-    var myChart = new Chart(chart, {
-      type: 'bar',
-      data: {
-        labels: jsonDataMois['data']['labels'],
-        datasets: [{
-          label: "Heureux",
-          fill: false,
-          lineTension: 0,
-          data: jsonDataMois['data']['datasets'][0]['data'],
-          pointBorderColor: "#4bc0c0",
-          borderColor: '#4bc0c0',
-          borderWidth: 2,
-          showLine: true,
-        }, {
-          label: "Stressé",
-          fill: false,
-          lineTension: 0,
-          startAngle: 2,
-          data: jsonDataMois['data']['datasets'][1]['data'],
-          // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-          backgroundColor: "transparent",
-          pointBorderColor: "#ff6384",
-          borderColor: '#ff6384',
-          borderWidth: 2,
-          showLine: true,
-        }, {
-          label: "Fatigué",
-          fill: false,
-          lineTension: 0,
-          startAngle: 2,
-          data: jsonDataMois['data']['datasets'][2]['data'],
-          // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-          backgroundColor: "transparent",
-          pointBorderColor: "#ffcd56",
-          borderColor: '#ffcd56',
-          borderWidth: 2,
-          showLine: true,
-        }]
-      },
-    });
+    myChart.data.datasets[0].data = jsonDataMois['data']['datasets'][0]['data'];
+    myChart.data.datasets[1].data = jsonDataMois['data']['datasets'][1]['data'];
+    myChart.data.datasets[2].data = jsonDataMois['data']['datasets'][2]['data'];
+    myChart.data.labels = jsonDataMois['data']['labels'];
+    myChart.update();
+    // var chart = document.getElementById('myChart');
+    // var myChart = new Chart(chart, {
+    //   type: 'bar',
+    //   data: {
+    //     labels: jsonDataMois['data']['labels'],
+    //     datasets: [{
+    //       label: "Heureux",
+    //       fill: false,
+    //       lineTension: 0,
+    //       data: jsonDataMois['data']['datasets'][0]['data'],
+    //       pointBorderColor: "#4bc0c0",
+    //       borderColor: '#4bc0c0',
+    //       borderWidth: 2,
+    //       showLine: true,
+    //     }, {
+    //       label: "Stressé",
+    //       fill: false,
+    //       lineTension: 0,
+    //       startAngle: 2,
+    //       data: jsonDataMois['data']['datasets'][1]['data'],
+    //       // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+    //       backgroundColor: "transparent",
+    //       pointBorderColor: "#ff6384",
+    //       borderColor: '#ff6384',
+    //       borderWidth: 2,
+    //       showLine: true,
+    //     }, {
+    //       label: "Fatigué",
+    //       fill: false,
+    //       lineTension: 0,
+    //       startAngle: 2,
+    //       data: jsonDataMois['data']['datasets'][2]['data'],
+    //       // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+    //       backgroundColor: "transparent",
+    //       pointBorderColor: "#ffcd56",
+    //       borderColor: '#ffcd56',
+    //       borderWidth: 2,
+    //       showLine: true,
+    //     }]
+    //   },
+    // });
 
 
 
 
-    // //  Chart ( jours )
-    var Chart2 = document.getElementById('myChart2').getContext('2d');
-    var chart = new Chart(Chart2, {
-      type: 'line',
-      data: {
-        labels: jourArray,
-        datasets: [{
-          label: "Stressé",
-          backgroundColor: jsonDataJours['datasets'][0]['backgroundColor'],
-          borderColor: jsonDataJours['datasets'][0]['borderColor'],
-          borderWidth: jsonDataJours['datasets'][0]['borderWidth'],
-          pointBorderColor: jsonDataJours['datasets'][0]['pointBorderColor'],
-          data: jsonDataJours['datasets'][0]['data'],
-          fill: jsonDataJours['datasets'][0]['fill'],
-          lineTension: jsonDataJours['datasets'][0]['lineTension'],
-        }, {
-          label: "Heureux",
-          fill: false,
-          lineTension: .4,
-          startAngle: 2,
-          data: jsonDataJours['datasets'][1]['data'],
-          // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-          backgroundColor: "transparent",
-          pointBorderColor: "#4bc0c0",
-          borderColor: '#4bc0c0',
-          borderWidth: 2,
-          showLine: true,
-        }, {
-          label: "Fatigué",
-          fill: false,
-          lineTension: .4,
-          startAngle: 2,
-          data: jsonDataJours['datasets'][2]['data'],
-          // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-          backgroundColor: "transparent",
-          pointBorderColor: "#ffcd56",
-          borderColor: '#ffcd56',
-          borderWidth: 2,
-          showLine: true,
-        }]
-      },
+    // // //  Chart ( jours )
+    chart.data.datasets[0].data = jsonDataJours['datasets'][0]['data'];
+    chart.data.datasets[1].data = jsonDataJours['datasets'][1]['data'];
+    chart.data.datasets[2].data = jsonDataJours['datasets'][2]['data'];
+    chart.data.labels = jourArray;
+    chart.update();
+    // var Chart2 = document.getElementById('myChart2').getContext('2d');
+    // var chart = new Chart(Chart2, {
+    //   type: 'line',
+    //   data: {
+    //     labels: jourArray,
+    //     datasets: [{
+    //       label: "Stressé",
+    //       backgroundColor: jsonDataJours['datasets'][0]['backgroundColor'],
+    //       borderColor: jsonDataJours['datasets'][0]['borderColor'],
+    //       borderWidth: jsonDataJours['datasets'][0]['borderWidth'],
+    //       pointBorderColor: jsonDataJours['datasets'][0]['pointBorderColor'],
+    //       data: jsonDataJours['datasets'][0]['data'],
+    //       fill: jsonDataJours['datasets'][0]['fill'],
+    //       lineTension: jsonDataJours['datasets'][0]['lineTension'],
+    //     }, {
+    //       label: "Heureux",
+    //       fill: false,
+    //       lineTension: .4,
+    //       startAngle: 2,
+    //       data: jsonDataJours['datasets'][1]['data'],
+    //       // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+    //       backgroundColor: "transparent",
+    //       pointBorderColor: "#4bc0c0",
+    //       borderColor: '#4bc0c0',
+    //       borderWidth: 2,
+    //       showLine: true,
+    //     }, {
+    //       label: "Fatigué",
+    //       fill: false,
+    //       lineTension: .4,
+    //       startAngle: 2,
+    //       data: jsonDataJours['datasets'][2]['data'],
+    //       // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+    //       backgroundColor: "transparent",
+    //       pointBorderColor: "#ffcd56",
+    //       borderColor: '#ffcd56',
+    //       borderWidth: 2,
+    //       showLine: true,
+    //     }]
+    //   },
 
-      // Configuration options
-      options: {
-        title: {
-          display: false
-        }
-      }
-    });
+    //   // Configuration options
+    //   options: {
+    //     title: {
+    //       display: false
+    //     }
+    //   }
+    // });
 
 
 
@@ -191,6 +203,139 @@ window.addEventListener('load', () => {
 // console.log("jsonDataJours : ", jsonDataJours);
 //END AJAX!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //END AJAX!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//ligne 247 à 259 change disposition du graphique jour et mois
+// var chart = document.getElementById('myChart');
+Chart.defaults.global.animation.duration = 2000; // Animation duration
+Chart.defaults.global.title.display = false; // Remove title
+Chart.defaults.global.title.text = "Chart"; // Title
+Chart.defaults.global.title.position = 'bottom'; // Title position
+Chart.defaults.global.defaultFontColor = '#999'; // Font color
+Chart.defaults.global.defaultFontSize = 10; // Font size for every label
+
+// Chart.defaults.global.tooltips.backgroundColor = '#FFF'; // Tooltips background color
+Chart.defaults.global.tooltips.borderColor = 'white'; // Tooltips border color
+Chart.defaults.global.legend.labels.padding = 0;
+Chart.defaults.scale.ticks.beginAtZero = true;
+Chart.defaults.scale.gridLines.zeroLineColor = 'rgba(255, 255, 255, 0.1)';
+Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.02)';
+Chart.defaults.global.legend.display = false;
+
+
+// Start chart
+//chart ( mois )
+var chart = document.getElementById('myChart');
+var myChart = new Chart(chart, {
+  type: 'bar',
+  data: {
+    labels: [],
+    datasets: [{
+      label: "Heureux",
+      fill: false,
+      lineTension: 0,
+      data: [],
+      pointBorderColor: "#4bc0c0",
+      borderColor: '#4bc0c0',
+      borderWidth: 2,
+      showLine: true,
+    }, {
+      label: "Stressé",
+      fill: false,
+      lineTension: 0,
+      startAngle: 2,
+      data: [],
+      // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+      backgroundColor: "transparent",
+      pointBorderColor: "#ff6384",
+      borderColor: '#ff6384',
+      borderWidth: 2,
+      showLine: true,
+    }, {
+      label: "Fatigué",
+      fill: false,
+      lineTension: 0,
+      startAngle: 2,
+      data: [],
+      // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+      backgroundColor: "transparent",
+      pointBorderColor: "#ffcd56",
+      borderColor: '#ffcd56',
+      borderWidth: 2,
+      showLine: true,
+    }]
+  },
+});
+
+
+
+
+// //  Chart ( jours )
+var Chart2 = document.getElementById('myChart2').getContext('2d');
+var chart = new Chart(Chart2, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: "Stressé",
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 79, 116)',
+      borderWidth: 2,
+      pointBorderColor: false,
+      data: [],
+      fill: false,
+      lineTension: .4,
+    }, {
+      label: "Heureux",
+      fill: false,
+      lineTension: .4,
+      startAngle: 2,
+      data: [],
+      // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+      backgroundColor: "transparent",
+      pointBorderColor: "#4bc0c0",
+      borderColor: '#4bc0c0',
+      borderWidth: 2,
+      showLine: true,
+    }, {
+      label: "Fatigué",
+      fill: false,
+      lineTension: .4,
+      startAngle: 2,
+      data: [],
+      // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+      backgroundColor: "transparent",
+      pointBorderColor: "#ffcd56",
+      borderColor: '#ffcd56',
+      borderWidth: 2,
+      showLine: true,
+    }]
+  },
+
+  // Configuration options
+  options: {
+    title: {
+      display: false
+    }
+  }
+});
+
+
+
+// function addData(chart, label, data) {
+//   chart.data.labels.push(label);
+//   chart.data.datasets.forEach((dataset) => {
+//     dataset.data.push(data);
+//   });
+//   chart.update();
+// }
+
+// function removeData(chart) {
+//   chart.data.labels.pop();
+//   chart.data.datasets.forEach((dataset) => {
+//     dataset.data.pop();
+//   });
+//   chart.update();
+// }
 
 $(function () {
 
@@ -258,140 +403,9 @@ $(function () {
 
   }());
 
-  //ligne 247 à 259 change disposition du graphique jour et mois
-  // var chart = document.getElementById('myChart');
-  Chart.defaults.global.animation.duration = 2000; // Animation duration
-  Chart.defaults.global.title.display = false; // Remove title
-  Chart.defaults.global.title.text = "Chart"; // Title
-  Chart.defaults.global.title.position = 'bottom'; // Title position
-  Chart.defaults.global.defaultFontColor = '#999'; // Font color
-  Chart.defaults.global.defaultFontSize = 10; // Font size for every label
-
-  // Chart.defaults.global.tooltips.backgroundColor = '#FFF'; // Tooltips background color
-  Chart.defaults.global.tooltips.borderColor = 'white'; // Tooltips border color
-  Chart.defaults.global.legend.labels.padding = 0;
-  Chart.defaults.scale.ticks.beginAtZero = true;
-  Chart.defaults.scale.gridLines.zeroLineColor = 'rgba(255, 255, 255, 0.1)';
-  Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.02)';
-  Chart.defaults.global.legend.display = false;
-
-
-  // Start chart
-  //chart ( mois )
-  // var chart = document.getElementById('myChart');
-  // var myChart = new Chart(chart, {
-  //   type: 'bar',
-  //   data: {
-  //     labels: [],
-  //     datasets: [{
-  //       label: "Heureux",
-  //       fill: false,
-  //       lineTension: 0,
-  //       data: [],
-  //       pointBorderColor: "#4bc0c0",
-  //       borderColor: '#4bc0c0',
-  //       borderWidth: 2,
-  //       showLine: true,
-  //     }, {
-  //       label: "Stressé",
-  //       fill: false,
-  //       lineTension: 0,
-  //       startAngle: 2,
-  //       data: [],
-  //       // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-  //       backgroundColor: "transparent",
-  //       pointBorderColor: "#ff6384",
-  //       borderColor: '#ff6384',
-  //       borderWidth: 2,
-  //       showLine: true,
-  //     }, {
-  //       label: "Fatigué",
-  //       fill: false,
-  //       lineTension: 0,
-  //       startAngle: 2,
-  //       data: [],
-  //       // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-  //       backgroundColor: "transparent",
-  //       pointBorderColor: "#ffcd56",
-  //       borderColor: '#ffcd56',
-  //       borderWidth: 2,
-  //       showLine: true,
-  //     }]
-  //   },
-  // });
-
-
-
-
-  // // //  Chart ( jours )
-  // var Chart2 = document.getElementById('myChart2').getContext('2d');
-  // var chart = new Chart(Chart2, {
-  //   type: 'line',
-  //   data: {
-  //     labels: [],
-  //     datasets: [{
-  //       label: "Stressé",
-  //       backgroundColor: [],
-  //       borderColor: [],
-  //       borderWidth: [],
-  //       pointBorderColor: [],
-  //       data: [],
-  //       fill: [],
-  //       lineTension: [],
-  //     }, {
-  //       label: "Heureux",
-  //       fill: false,
-  //       lineTension: .4,
-  //       startAngle: 2,
-  //       data: [],
-  //       // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-  //       backgroundColor: "transparent",
-  //       pointBorderColor: "#4bc0c0",
-  //       borderColor: '#4bc0c0',
-  //       borderWidth: 2,
-  //       showLine: true,
-  //     }, {
-  //       label: "Fatigué",
-  //       fill: false,
-  //       lineTension: .4,
-  //       startAngle: 2,
-  //       data: [],
-  //       // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-  //       backgroundColor: "transparent",
-  //       pointBorderColor: "#ffcd56",
-  //       borderColor: '#ffcd56',
-  //       borderWidth: 2,
-  //       showLine: true,
-  //     }]
-  //   },
-
-  //   // Configuration options
-  //   options: {
-  //     title: {
-  //       display: false
-  //     }
-  //   }
-  // });
-
-
-  // function addData(chart, label, data) {
-  //   chart.data.labels.push(label);
-  //   chart.data.datasets.forEach((dataset) => {
-  //     dataset.data.push(data);
-  //   });
-  //   chart.update();
-  // }
-
-  // function removeData(chart) {
-  //   chart.data.labels.pop();
-  //   chart.data.datasets.forEach((dataset) => {
-  //     dataset.data.pop();
-  //   });
-  //   chart.update();
-  // }
-
-
 });
+
+
 
 
 var nomjour = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi ", "Dimanche"];
