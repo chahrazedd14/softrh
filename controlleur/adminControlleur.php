8c346ef71b25ca1af29cb91158c26f22c5c8894b
Mr-Personne <h.sacha07@gmail.com>
2020-02-10 17:01:33 +0000

    sent twig variable with subProject variable echo ->render('admin.html', ['subProject' => ]) (so we only have to change one variable in the futur and not all urls)

--- controlleur/adminControlleur.php
+++ controlleur/adminControlleur.php
@@ -93,7 +93,7 @@ switch ($action) {
         $loader = new \Twig\Loader\FilesystemLoader('views');
         $twig = new \Twig\Environment($loader);
         // $template = $twig->load('admin-test.html.twig');
-        echo $twig->render('admin.html', ['var1' => 'variables', 'var2' => 'here']);
+        echo $twig->render('admin.html', ['subProject' => $subProject]);
         break;
 
 
