714cffa83d9c8866cb0f8ad5713da6d74d2d65b8
Mr-Personne <h.sacha07@gmail.com>
2020-02-10 08:59:59 +0000

    ajout redirection vers /employe si [3] n est pas isset

--- controlleur/employeControlleur.php
+++ controlleur/employeControlleur.php
@@ -26,7 +26,7 @@ if ($userHasVoted == "") {
 // // $template = $twig->load('admin-test.html.twig');
 // echo $twig->render('employe-test.html.twig', ['bonjour' => 'sa marche employe', 'var2' => 'here']);
 // echo 'ENTER PAPGE Employe';
-
+// echo "ceci est l'action : ".$action;
 switch ($action) {
     case 'default':
         // require_once 'views/employe.html';
@@ -41,15 +41,19 @@ switch ($action) {
         // print_r($today);
         $uri = $_SERVER['REQUEST_URI'];
         $expUri = explode("/", $uri);
+
+        if(!isset($expUri[3])){
+            header('Location: /employe');
+            exit();
+        }
         // print_r($_SESSION);
         //si le chiffre du vote est entre 1 et 3 compris :
-        if ($expUri[3] < 1 || $expUri[3] > 4) {
-            
+        if ($expUri[3] < 1 || $expUri[3] > 4 || $expUri[3] == "") {
+
             $loader = new \Twig\Loader\FilesystemLoader('views');
             $twig = new \Twig\Environment($loader);
             echo $twig->render('employe.html', ['emoticons' => "emoticons"]);
-        }
-         else {
+        } else {
 
             $selectedHumeur = $expUri[3];
             $selectedService = $_SESSION['id_service'];
@@ -80,7 +84,7 @@ switch ($action) {
         $loader = new \Twig\Loader\FilesystemLoader('views');
         $twig = new \Twig\Environment($loader);
         // $template = $twig->load('admin-test.html.twig');
-        echo $twig->render('404.html.php', ['hasVoted' => "Merci d'avoir voté!"]);
+        echo $twig->render('404.html.php', ['message' => "404"]);
         break;
 }
 
